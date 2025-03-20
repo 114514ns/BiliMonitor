8cbad2d4b8ff934f3a61527885a3407923efb486
114514ns <lua6705@gmail.com>
2025-03-21 00:30:18 +0800

    1.拆出来的文件忘记添加到git了

---
+++ alist.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
+	"github.com/bytedance/sonic"
+	"io"
+	"log"
+	"mime/multipart"
+	"net/http"
+	"os"
+	"path/filepath"
+)
+
+func UploadFile(path string, alistPath string) error {
+	file, err := os.Open(path)
+	if err != nil {
+		log.Printf("打开文件失败: %w", err)
+	}
+	defer file.Close()
+
+	bodyReader, bodyWriter := io.Pipe() // 创建 Pipe
+	writer := multipart.NewWriter(bodyWriter)
+
+	go func() {
+		defer bodyWriter.Close()
+		part, err := writer.CreateFormFile("file", filepath.Base(path))
+		if err != nil {
+			bodyWriter.CloseWithError(fmt.Errorf("创建表单文件失败: %w", err))
+			return
+		}
+
+		_, err = io.Copy(part, file)
+		if err != nil {
+			bodyWriter.CloseWithError(fmt.Errorf("复制文件数据失败: %w", err))
+			return
+		}
+
+		writer.Close()
+	}()
+
+	req, err := http.NewRequest("PUT", config.AlistServer+"api/fs/form", bodyReader)
+	if err != nil {
+		log.Println("创建请求失败:", err)
+	}
+
+	req.Header.Set("Content-Type", writer.FormDataContentType())
+	req.Header.Set("Authorization", GetAlistToken())
+	req.Header.Set("File-Path", alistPath)
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Println("上传请求失败: %w", err.Error())
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Println("读取响应失败: %w", err)
+	}
+
+	fmt.Printf("[%s] %d %s\n", alistPath, resp.StatusCode, string(body))
+	return nil
+}
+func GetAlistToken() string {
+	type LoginResponse struct {
+		Data struct {
+			Token string `json:"token"`
+		} `json:"data"`
+	}
+	type LoginRequest struct {
+		Username string `json:"username"`
+		Password string `json:"password"`
+	}
+	var sum = sha256.Sum256([]byte(config.AlistPass + "-https://github.com/alist-org/alist"))
+	var req = LoginRequest{Username: config.AlistUser, Password: hex.EncodeToString(sum[:])}
+	alist, err := client.R().SetBody(req).Post(config.AlistServer + "api/auth/login/hash")
+
+	if err != nil {
+		log.Println(err)
+	}
+	var res = LoginResponse{}
+	sonic.Unmarshal(alist.Body(), &res)
+	return res.Data.Token
+}
---
+++ build.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+
+APP_NAME="BiliMonitor"
+
+OUTPUT_DIR="build"
+mkdir -p "$OUTPUT_DIR"
+
+
+declare -A TARGETS=(
+  ["windows_amd64"]="GOOS=windows GOARCH=amd64"
+  ["linux_amd64"]="GOOS=linux GOARCH=amd64"
+  ["linux_mips"]="GOOS=linux GOARCH=mips"
+  ["linux_arm64"]="GOOS=linux GOARCH=arm64"
+  ["darwin_amd64"]="GOOS=darwin GOARCH=amd64"
+  ["darwin_arm64"]="GOOS=darwin GOARCH=arm64"
+)
+
+for target in "${!TARGETS[@]}"; do
+  eval "${TARGETS[$target]}" go build -o "$OUTPUT_DIR/$APP_NAME-$target" .
+  if [ $? -eq 0 ]; then
+    echo "Build successful: $OUTPUT_DIR/$APP_NAME-$target"
+    if [[ "$target" != darwin_* ]]; then
+        upx --best --lzma "$OUTPUT_DIR/$APP_NAME-$target"
+    fi
+  else
+    echo "Build failed: $target"
+  fi
+done
+
