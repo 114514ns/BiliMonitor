e85eaa3d3490ee4b91d9347e60feba14bebcd964
114514ns <lua6705@gmail.com>
2025-04-02 00:31:30 +0800

    1.移除未使用的依赖

--- Page/vite.config.js
+++ Page/vite.config.js
@@ -2,18 +2,20 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default {
+export default defineConfig({
   plugins: [react()],
   server: {
-    proxy:{
-      '/': {
-        target: 'http://127.0.0.1:8080',
+    proxy: {
+      "/": {
+        target: "http://127.0.0.1:8081",
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
       },
-    }
-
-  }
-
-
-};
+    },
+  },
+  build: {
+    sourcemap: true,
+  },
+  esbuild: {
+    sourcemap: true,
+  },
+});
