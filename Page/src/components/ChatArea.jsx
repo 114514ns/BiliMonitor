bfe41d86ecfce3c4b102d9b427f6382a76c22609
114514ns <lua6705@gmail.com>
2025-04-01 14:22:34 +0800

    配置github action

---
+++ Page/src/components/ChatArea.jsx
@@ -0,0 +1,152 @@
+import React, {useEffect, useRef, useState} from 'react';
+import classes from "../pages/ChatPage.module.css";
+import {Avatar, Card, CardBody, CardFooter, Chip} from "@heroui/react";
+import {CheckIcon} from "../pages/ChatPage";
+import axios from "axios";
+import {useVirtualizer} from "@tanstack/react-virtual";
+import List from 'rc-virtual-list';
+import {motion} from "framer-motion";
+import {Listbox, ListboxItem} from "@heroui/react";
+function GiftPart(props) {
+    return (
+        <div className={classes.giftArea}>
+            <p>{props.name}：CNY {props.price}</p>
+            <img src={props.img} alt="" style={{objectFit: "cover",width:"70px"}} />
+        </div>
+    );
+}
+function ChatArea(props) {
+    const chatRef = useRef(null);
+    const [last, setLast] = useState("");
+    const [message, setMessage] = useState([]);
+    const lastRef = useRef(null);
+    const host = location.hostname;
+    const port = location.port;
+    const protocol = location.protocol.replace(":", "");
+
+    useEffect(() => {
+        lastRef.current = last;
+    }, [last]);
+
+
+    const refresh = () => {
+        axios
+            .get(`${protocol}://${host}:${port}/history?room=${props.room}&last=${lastRef.current}`)
+            .then((res) => {
+                if (res.data.data.length !== 0) {
+                    const newData = res.data.data.filter((item) => item.ActionName !== "enter");
+
+                    if (newData.length > 0) {
+                        setMessage((prev) => {
+                            const merged = [...prev, ...newData];
+                            return JSON.stringify(merged) === JSON.stringify(prev) ? prev : merged;
+                        });
+
+                        setLast(res.data.data[res.data.data.length - 1].UUID);
+                    }
+                }
+            });
+    };
+
+    useEffect(() => {
+        if (chatRef.current) {
+            chatRef.current.scrollTop = chatRef.current.scrollHeight;
+        }
+    }, [message]);
+
+    useEffect(() => {
+        setLast("");
+        setMessage([]);
+        refresh();
+
+        const interval = setInterval(() => {
+            refresh();
+        }, 1000);
+
+        return () => {
+            clearInterval(interval);
+            setMessage([]);
+        };
+    }, [props.room]);
+
+    return (
+        <div>
+            <div className={classes.chatColumn}>
+                <div style={{ height: "100%", width: "100%", overflow: "auto" }} ref={chatRef}>
+                        <Listbox
+                            isVirtualized
+                            autoFocus={'first'}
+                            label={"Select from 1000 items"}
+                            placeholder="Select..."
+                            hideSelectedIcon
+                            variant={'light'}
+                            virtualization={{
+                                maxListboxHeight: 800,
+                                itemHeight: 160,
+                            }}
+                        >
+                            {message.map((item, index) => (
+                                <ListboxItem key={index} value={item.value} textValue={'1'}>
+                                    <ChatItem item={item} />
+                                </ListboxItem>
+                            ))}
+                        </Listbox>
+                </div>
+            </div>
+        </div>
+    );
+}
+function ChatItem(props) {
+
+    var item = props.item;
+    var virtualRow = props.row
+    return (
+        <Card style={{width: "90%",marginTop:"10px"}}
+              isHoverable
+              key={`${item.UUID}`}
+
+        >
+            <CardBody>
+                <div style={{ display: "flex" }}>
+                    <img
+                        src={item.Face}
+                        style={{width:'40px',height:'40px',borderRadius:'50%'}}
+                        onClick={() => toSpace(UID)}
+                    />
+                    <div>
+                        <div className={classes.nameRow}>
+                            <p>{item.FromName}</p>
+                            {item.MedalName && (
+                                <Chip
+                                    startContent={<CheckIcon size={18} />}
+                                    variant="faded"
+                                    style={{ background: item.MedalColor,color:'white',marginLeft:'8px'}}
+                                >
+                                    {item.MedalName}
+                                    <span className="ml-2 text-xs font-bold px-2 py-0.5 rounded-full">
+                                                            {item.MedalLevel}
+                                                        </span>
+                                </Chip>
+                            )}
+                        </div>
+                        <div style={{ marginLeft: "8px" }}>
+                            {item.ActionName === "msg" ? (
+                                <span className="messageText" >{item.Extra}</span>
+                            ) : (
+                                <GiftPart name={item.GiftName} img={item.GiftPicture} price={item.GiftPrice.Float64} />
+                            )}
+                        </div>
+                    </div>
+                </div>
+            </CardBody>
+            <CardFooter className={classes.msgTime}>
+                <p>
+                    {String(new Date(item.CreatedAt).getHours()).padStart(2, '0')}:
+                    {String(new Date(item.CreatedAt).getMinutes()).padStart(2, '0')}
+                </p>
+            </CardFooter>
+        </Card>
+    )
+}
+
+export default ChatArea;
