1be9eea807e321c0dfd5f4ad6d1ab2f6bd97931d
114514ns <lua6705@gmail.com>
2025-04-01 15:39:25 +0800

    1.正确设置gin的端口

--- api.go
+++ api.go
@@ -330,7 +330,7 @@ func InitHTTP() {
 		})
 	})
 
-	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	r.Run(":" + strconv.Itoa(int(config.Port))) // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
 
 func CORSMiddleware() gin.HandlerFunc {
