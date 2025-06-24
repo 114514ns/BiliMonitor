78e06a4e674be512b0bdaa24f04308d98566b02e
114514ns <lua6705@gmail.com>
2025-06-25 00:18:32 +0800

    1.更新README 2.重新实现刷新所有主播信息的逻辑，应该更快了 3.直播详情界面支持在本次直播内按用户筛选弹幕 4.主播列表界面支持使用SQL语句进行复杂查询 5.添加获取某用户所有粉丝牌的接口 6.优化刷新所有主播粉丝量的逻辑

--- Page/vite.config.js
+++ Page/vite.config.js
@@ -8,13 +8,14 @@ export default defineConfig({
     proxy: {
 
       "/api": {
-        target: "http://127.0.0.1:8080",
+        target: "http://127.0.0.1:8081",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       },
 
     },
-    port:5173
+    port:5173,
+    allowedHosts:['live-dev.ikun.dev']
   },
   build: {
     sourcemap: true,
