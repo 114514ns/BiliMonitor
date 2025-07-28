5f886fe2e9b2b3a99e1ab2c90999e78e6dea23b9
114514ns <lua6705@gmail.com>
2025-07-28 23:27:25 +0800

    1.忘记加到git了

---
+++ Page/src/components/ActionTable.jsx
@@ -0,0 +1,130 @@
+import React, {useEffect} from 'react';
+import {
+    Chip,
+    Pagination,
+    Table,
+    TableBody,
+    TableCell,
+    TableColumn,
+    TableHeader,
+    TableRow,
+    Tooltip
+} from "@heroui/react";
+import {CheckIcon} from "../pages/ChatPage";
+import HoverMedals from "./HoverMedals";
+
+function ActionTable(props) {
+
+    const [currentPage, setCurrentPage] = React.useState(1);
+
+
+
+    const pageSize = 10
+
+    useEffect(() => {
+        props.handlePageChange(1.10)
+    },[])
+
+    const columns = [
+        {
+            title: 'Name',
+            dataIndex: 'FromName',
+            key: 'UserName',
+            render: (text, record) => (
+                <span style={{cursor: 'pointer'}} onClick={() => {
+                    window.open("https://space.bilibili.com/" + record.FromId)
+                }}>
+        {text}{console.log(record)}
+      </span>
+            )
+        },
+        {
+            title: 'Title',
+            dataIndex: 'Liver',
+            key: 'Title',
+        },
+        {
+            title: 'Time',
+            dataIndex: 'CreatedAt',
+            key: 'StartAt',
+        },
+        {
+            title: 'Money',
+            dataIndex: 'GiftPrice',
+            key: 'Money',
+            sorter: true,
+
+        },
+        {
+            title: 'Message',
+            dataIndex: 'Extra',
+            key: 'Message'
+        }
+    ]
+
+    return (
+        <div>
+            <Table bottomContent={
+                <div className="flex w-full justify-center">
+                    <Pagination
+                        isCompact
+                        showControls
+                        showShadow
+                        color="secondary"
+                        page={currentPage}
+                        total={Math.ceil(props.total / pageSize)}
+                        onChange={(page) => props.handlePageChange(page, pageSize)}
+                    />
+                </div>
+            } isStriped>
+
+                <TableHeader>
+                    {columns.map((col, index) => (
+                        <TableColumn key={index}>{col.title}</TableColumn>
+
+                    ))}
+                </TableHeader>
+                <TableBody>
+
+                    {props.dataSource.map((item, index) => (
+                        <TableRow key={index} onClick={() => {
+
+                        }}>
+                            <TableCell>
+                                <Tooltip content={
+                                    <HoverMedals mid={item.FromId}/>
+                                } delay={400}>
+                                    <div className={'flex'} onClick={() => {
+                                        toSpace(item.FromId)
+                                    }}>
+                                        {item.FromName}
+                                        {item.MedalLevel != 0 ?                                     <Chip
+                                            className={'basis-64'}
+                                            startContent={<CheckIcon size={18}/>}
+                                            variant="faded"
+                                            onClick={() => {
+                                                toSpace(props.props.LiverID);
+                                            }}
+                                            style={{background: getColor(item.MedalLevel), color: 'white', marginLeft: '8px'}}
+                                        >
+                                            {item.MedalName}
+                                            <span className="ml-2 text-xs font-bold px-2 py-0.5 rounded-full">
+                                                            {item.MedalLevel}
+                                                        </span>
+                                        </Chip>:<></>}
+                                    </div>
+                                </Tooltip>
+                            </TableCell>
+                            <TableCell>{item.ToName}</TableCell>
+                            <TableCell>{item.CreatedAt}</TableCell>
+                            <TableCell>{item.GiftPrice.Float64}</TableCell>
+                            <TableCell>{item.GiftName || item.Extra}</TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </div>
+    );
+}
+
+export default ActionTable;
---
+++ Page/src/components/HoverMedals.jsx
@@ -0,0 +1,54 @@
+import React, {useEffect, useState} from 'react';
+import axios from "axios";
+import {Avatar, Chip} from "@heroui/react";
+
+function HoverMedals(props) {
+    var mid = props.mid
+    var [data,setData] = useState([])
+    useEffect(() => {
+        axios.get(`${protocol}://${host}:${port}/api/medals?mid=${mid}`).then((response) => {
+            if (response.data.list == null) {
+                response.data.list = []
+            }
+            response.data.list.sort((a,b) => {
+                return a.Score < b.Score
+            })
+            setData(response.data.list)
+        })
+    },[])
+    return (
+
+        <div className={'flex flex-col overflow-scroll fansMedal overflow-x-hidden'} style={{maxHeight:'600px'}}>
+            {data.map((item, index) => (
+                <div key={index} className={'mt-2'}>
+                    <p className={'font-medium'}>{item.Liver}</p>
+
+                    <div className={'flex flex-row align-middle mt-2'}>
+                        <Avatar
+                            src={`${protocol}://${host}:${port}${import.meta.env.PROD ? '' : '/api'}/face?mid=${item.LiverID}`}
+                            onClick={() => {
+                                toSpace(item.LiverID);
+                            }}/>
+
+                        {item.Level ?              <Chip
+                            startContent={item.Type?<img  src={getGuardIcon(item.Type)} className={'w-6 h-6'}/>:<div/>}
+                            variant="faded"
+                            onClick={() => {
+                                toSpace(item.LiverID);
+                            }}
+                            style={{background: getColor(item.Level), color: 'white', marginLeft: '8px'}}
+                        >
+                            {item.MedalName}
+                            <span className="ml-2 text-xs font-bold px-2 py-0.5 rounded-full">
+                                                            {item.Level}
+                                                        </span>
+                        </Chip>:<></>}
+                    </div>
+                </div>
+            ))}
+
+        </div>
+    )
+}
+
+export default HoverMedals;
